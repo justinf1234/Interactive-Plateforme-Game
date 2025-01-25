85e89c60909fd04ad5d1019bedb89fc149da12e0
Justin Ferland <justlandry1@gmail.com>
2025-01-25 18:32:39 -0500

    Performing more initialization

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+
+
