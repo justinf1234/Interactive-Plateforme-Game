2d85cc8e489d1927a161ecab62dd4185badc8aed
Justin Ferland <justlandry1@gmail.com>
2025-01-25 21:48:27 -0500

    Add right boundary collision handling

--- script.js
+++ script.js
@@ -49,5 +49,9 @@ class Player {
     if (this.position.x < this.width) {
       this.position.x = this.width;
     }
+
+    if (this.position.x >= canvas.width - this.width * 2) {
+      this.position.x = canvas.width - this.width * 2;
+    }
   }
 }
